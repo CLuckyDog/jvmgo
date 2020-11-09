bf825baa3bd307952936986113776a8edd5b01cb
unknown <Administrator@note190342.Hoperun.com>
2020-11-09 22:08:28 +0800

    提交test

--- test/test.go
+++ test/test.go
@@ -2,11 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
-	test(1, 2, 3, 4, 5, 9)
+type Book struct {
+	name string
+	count int
 }
 
-func test(args ...int) {
+func main() {
+	//test1(1, 2, 3, 4, 5, 9)
+	sb := make([]Book,5)
+	for i := 1; i < 5; i++ {
+		sb[i] =readBook(sb)
+	}
+}
+
+func readBook(b Book) Book{
+	fmt.Printf("正在读书: %v\n",b.name)
+}
+
+func test1(args ...int) {
 	fmt.Println("len(args):", len(args))
 	// test(args[2:]...)
 }
